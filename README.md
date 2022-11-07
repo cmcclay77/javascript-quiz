84a21be930d57fe45de96d19348aae170ae560d4
Chris McClay <cmcclay77@gmail.com>
2022-11-07 10:17:40 -0500

    updated code and readme file

--- README.md
+++ README.md
@@ -1,1 +1,41 @@
-# javascript-quiz
+# JavaScript Code Quiz
+
+# Description
+
+This is a short multiple choice quiz designed using HTML, CSS and JavaScript.  The questions are from an array of objects for the user to answer in the browser.  After an anwser is selected the user can evaluate to see if the correct answer is chosen and then click next to move to another question.  After the quiz the user can store their initials and see high scores on another page.
+
+## Usage
+
+The live link is:
+
+## Screenshot
+
+
+
+## Credits
+
+* I would like to thank all the people who helped me with this project to include friends and tutors
+* I would also thank the instructional staff who instructed us on JavaScript to be able to complete the project for the Code Quiz.
+
+## License
+
+Please refer to the MIT license in the Repo.
+
+## Badges
+
+* HTML
+* CSS
+* JavaScript
+* Dev Tools
+
+## Features
+
+Some of the features of this project include the use of functions, loops and assigning variables in JavaScript.  The end result of the code is the creation of a JavaScript Code Quiz.  Some comments were also added.
+
+## How to Contribute
+
+N/A 
+
+## Tests
+
+N/A 
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,104 +1,12 @@
-/* var minutesLabel = document.getElementById("minutes");
- var secondsLabel = document.getElementById("seconds");
- var endOfTimer = document.getElementById("timerEnd");
- var totalSeconds = 5;
-
- function start(runOnce, totalSeconds) {
-     setInterval(setTime(totalSeconds), 1000);
- }
-
- function setTime(totalSeconds) {
-     if(totalSeconds > 0 && runOnce == true) {
-         --totalSeconds;
-         secondsLabel.innerHTML = pad(totalSeconds % 60);
-         minutesLabel.innerHTML = pad(parseInt(totalSeconds / 60)); 
-     } else if(totalSeconds < 0) {
-         return timerEnd(false);
-     }
-   console.log('totalsecondds', totalSeconds);
-
- } 
-
- function timerEnd(runOnce) {
-    if(confirm('You have run out of time.  Would you like to start again?') == true) {
-         console.log(runOnce);
-         start(true, 5);
-     } else {
-         start(false, -1);
-     }
- }
-
- function pad(val) {
-   var valString = val + "";
-   if (valString.length < 2) {
-     return "0" + valString;
-   } else {
-     return valString;
-   }
- }
-
- var timerEl = document.getElementById('countdown');
- var mainEl = document.getElementById('main');
-
- var displayMessage = "Your time has run out!  Would you like to try again?";
-  
-
-  Timer that counts down from 5
- function countdown() {
-   var timeLeft = 5;
-
-    Use the `setInterval()` method to call a function to be executed every 1000 milliseconds
-   var timeInterval = setInterval(function () {
-      As long as the `timeLeft` is greater than 1
-     if (timeLeft > 1) {
-        Set the `textContent` of `timerEl` to show the remaining seconds
-       timerEl.textContent = timeLeft + ' seconds remaining';
-        Decrement `timeLeft` by 1
-       timeLeft--;
-     } else if (timeLeft === 1) {
-        When `timeLeft` is equal to 1, rename to 'second' instead of 'seconds'
-       timerEl.textContent = timeLeft + ' second remaining';
-       timeLeft--;
-     } else {
-        Once `timeLeft` gets to 0, set `timerEl` to an empty string
-       timerEl.textContent = '';
-        Use `clearInterval()` to stop the timer
-       clearInterval(timeInterval);
-        Call the `displayMessage()` function
-       displayMessage("Your time has run out!  Would you like to try again?");
-     }
-   }, 1000);
- }
-
- Displays the message one word at a time
- function displayMessage() {
-   var wordCount = 0;
- }
-
-Uses the `setInterval()` method to call a function to be executed every 1000 milliseconds
-   var msgInterval = setInterval(function () {
-      If there are no more words left in the message
-     if (words[wordCount] === undefined) {
-       Use `clearInterval()` to stop the timer
-       clearInterval(msgInterval);
-      } else {
-        Display one word of the message
-        mainEl.textContent = words[wordCount];
-        wordCount++;
-     }
-   }, 1000);
-
- countdown();*/
-
-//Questions will be asked
-
 var timer;
 var timerCount;
 var timerElement = document.querySelector('#clock-timer');
 var timerText = document.querySelector('#timer-text');
+
+
 function startTimer() {
   // Sets timer
-  timerCount = 5;
+  timerCount = 120;
   timer = setInterval(function() {
     timerCount--;
     timerText.textContent = timerCount;
